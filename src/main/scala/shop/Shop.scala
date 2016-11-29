fba9d681c11295918ea1ed4225ef6ccc61204dea
Jules Gosnell <Jules.x.Gosnell@barclayswealth.com>
2016-11-29 19:56:28 +0000

    fix the test

--- src/main/scala/shop/Shop.scala
+++ src/main/scala/shop/Shop.scala
@@ -8,6 +8,6 @@ case class Item(name: String, cost: BigDecimal)
 class Shop {
 
   // sum 'items' by 'cost' - NYI
-  def checkOut(items: List[Item]): BigDecimal = ???
+  def checkOut(items: List[Item]): BigDecimal = items.map(_.cost).fold(BigDecimal(0))(_ + _)
 
 }
