9033013ee346cc89741ccada2e519b3018285c44
Jules Gosnell <Jules.x.Gosnell@barclayswealth.com>
2016-11-29 23:59:39 +0000

    add ability to checkOut by item name - what should we do about bad names - return them so code above can make that decision

--- src/main/scala/shop/Shop.scala
+++ src/main/scala/shop/Shop.scala
@@ -4,10 +4,21 @@ package shop
 
 case class Item(name: String, cost: BigDecimal)
 
-
-class Shop {
+case class Shop(stock: List[Item]) {
 
   // sum 'items' by 'cost' - NYI
   def checkOut(items: List[Item]): BigDecimal = items.map(_.cost).fold(BigDecimal(0))(_ + _)
 
+  val stockByName: Map[String, Item] = stock.foldLeft(Map.empty[String, Item]){ (acc, item) => acc + (item.name -> item)}
+
+  // resolve names to items, returning tuple of checkOut(items) and unresolved names
+  def checkOutByName(names: List[String]): (BigDecimal, List[String]) = {
+    val (is, ns) = names.
+      map(name => stockByName.get(name).fold[Either[Item, String]](Right(name))(item => Left(item))).
+      foldLeft[(List[Item], List[String])]((Nil, Nil))((acc, e) => {
+        val (iacc, nacc) = acc
+        e.fold(i => (i :: iacc, nacc), n => (iacc, n :: nacc))
+      })
+    (checkOut(is), ns.reverse)
+  }
 }
--- src/test/scala/shop/ShopSpec.scala
+++ src/test/scala/shop/ShopSpec.scala
@@ -8,9 +8,15 @@ class ShopSpec extends FlatSpec with TypeCheckedTripleEquals {
   val apple  = Item("Apple",  BigDecimal(0.60))
   val orange = Item("Orange", BigDecimal(0.25))
 
+  val stock = List(apple, orange)
+  val shop = Shop(stock)
+
   "checkout()" should "sum a list of items" in {
-    val shop = new Shop()
-    assert(shop.checkOut(apple :: apple :: orange :: apple :: Nil) == BigDecimal(2.05), "checkout failed")
+    assert(shop.checkOut(apple :: apple :: orange :: apple :: Nil) === BigDecimal(2.05), "checkout failed")
+  }
+
+  "checkoutByName()" should "return the checkout() of resolvable names and a list of unresolvable names" in {
+    assert(shop.checkOutByName("Apple" :: "Apple" :: "Lemon" :: "Orange" :: "Apple" :: "Lime" :: Nil) === (BigDecimal(2.05), List("Lemon", "Lime")), "checkoutByName failed")
   }
 
 }
