7a049b5dea2b5ffdcda8abc26dbceb1eb55133ff
Jules Gosnell <Jules.x.Gosnell@barclayswealth.com>
2016-11-30 00:45:23 +0000

    add support for special offers

--- src/main/scala/shop/Shop.scala
+++ src/main/scala/shop/Shop.scala
@@ -1,13 +1,34 @@
 package shop
 
-// let's represent items by composition, rather than heading straight for a sealed trait...
-
 case class Item(name: String, cost: BigDecimal)
 
-case class Shop(stock: List[Item]) {
+case class SpecialOffer(itemName: String, f: List[Item] => List[Item])
+
+object Shop {
+
+  def bogof(items: List[Item]): List[Item] = items match {
+    case i1 :: i2 :: is => i1 :: bogof(is)
+    case is => is
+  }
+
+  def threeForTwo(items: List[Item]): List[Item] = items match {
+    case i1 :: i2 :: i3 :: is => i1 :: i2 :: threeForTwo(is)
+    case is => is
+  }
+
+  def applySpecialOffers(offers: Map[String, List[SpecialOffer]], items: Map[String, List[Item]]): List[Item] =
+    items.map { case (k, v) => k -> offers.getOrElse(k, Nil).foldLeft(v)((is, so) => so.f(is))}.values.flatten.toList
+
+}
+
+case class Shop(stock: List[Item], specialOffers: List[SpecialOffer] = Nil) {
+
+  import Shop._
+
+  val specialOffersByItemName: Map[String, List[SpecialOffer]] = specialOffers.groupBy(_.itemName)
 
   // sum 'items' by 'cost' - NYI
-  def checkOut(items: List[Item]): BigDecimal = items.map(_.cost).fold(BigDecimal(0))(_ + _)
+  def checkOut(items: List[Item]): BigDecimal = applySpecialOffers(specialOffersByItemName, items.groupBy(_.name)).map(_.cost).fold(BigDecimal(0))(_ + _)
 
   val stockByName: Map[String, Item] = stock.foldLeft(Map.empty[String, Item]){ (acc, item) => acc + (item.name -> item)}
 
--- src/test/scala/shop/ShopSpec.scala
+++ src/test/scala/shop/ShopSpec.scala
@@ -9,14 +9,46 @@ class ShopSpec extends FlatSpec with TypeCheckedTripleEquals {
   val orange = Item("Orange", BigDecimal(0.25))
 
   val stock = List(apple, orange)
-  val shop = Shop(stock)
 
   "checkout()" should "sum a list of items" in {
+    val shop = Shop(stock)
     assert(shop.checkOut(apple :: apple :: orange :: apple :: Nil) === BigDecimal(2.05), "checkout failed")
   }
 
   "checkoutByName()" should "return the checkout() of resolvable names and a list of unresolvable names" in {
+    val shop = Shop(stock)
     assert(shop.checkOutByName("Apple" :: "Apple" :: "Lemon" :: "Orange" :: "Apple" :: "Lime" :: Nil) === (BigDecimal(2.05), List("Lemon", "Lime")), "checkoutByName failed")
+  }
+
+  import Shop._
+
+  "buy-one-get-one-free" should "reduce items accordingly" in {
+    assert(bogof(List()).length === 0)
+    assert(bogof(List(apple)).length === 1)
+    assert(bogof(List(apple, apple)).length === 1)
+    assert(bogof(List(apple, apple, apple)).length === 2)
+    assert(bogof(List(apple, apple, apple, apple)).length === 2)
+    assert(bogof(List(apple, apple, apple, apple, apple)).length === 3)
+  }
+
+  "three-for-the-price-of-two" should "reduce items accordingly" in {
+    assert(threeForTwo(List()).length === 0)
+    assert(threeForTwo(List(orange)).length === 1)
+    assert(threeForTwo(List(orange, orange)).length === 2)
+    assert(threeForTwo(List(orange, orange, orange)).length === 2)
+    assert(threeForTwo(List(orange, orange, orange, orange)).length === 3)
+    assert(threeForTwo(List(orange, orange, orange, orange, orange)).length === 4)
+    assert(threeForTwo(List(orange, orange, orange, orange, orange, orange)).length === 4)
+    assert(threeForTwo(List(orange, orange, orange, orange, orange, orange, orange)).length === 5)
+  }
+
+  "special offers" should "be applied on checkout " in {
+    val specialOffers = List(SpecialOffer("Apple", bogof), SpecialOffer("Orange", threeForTwo))
+    val shop = Shop(stock, specialOffers)
+
+    assert(shop.checkOut(apple :: apple :: Nil) === BigDecimal(0.60), "checkout with special offers failed")
+    assert(shop.checkOut(orange :: orange :: orange :: Nil) === BigDecimal(0.50), "checkout with special offers failed")
+    assert(shop.checkOut(orange :: orange :: apple :: orange :: apple :: Nil) === BigDecimal(1.10), "checkout with special offers failed")
   }
 
 }
